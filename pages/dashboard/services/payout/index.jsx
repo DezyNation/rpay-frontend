358683588905d8e5829b3d39d8d5d4b2552ec7b4
DezyNation <dezynationindia@gmail.com>
2023-04-18 20:12:23 +0530

    payscope

--- pages/dashboard/services/payout/index.jsx
+++ pages/dashboard/services/payout/index.jsx
@@ -71,7 +71,7 @@ const PaymentReceipt = ({ amount, payout_id, name, account }) => {
 }
 
 const Payout = () => {
-    const [provider, setProvider] = useState("razorpay")
+    const [provider, setProvider] = useState("payscope")
     const [serviceId, setServiceId] = useState("25")
     const { isOpen, onClose, onOpen } = useDisclosure()
     const Toast = useToast()
